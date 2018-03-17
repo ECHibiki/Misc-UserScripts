a73b59d15fb1993834f3b39cfa7986f4ba5cecc9
KNG <34425597+ECHibiki@users.noreply.github.com>
2018-03-17 05:08:00 -0400

    line break bugfix

--- Press '.' and do Quoting things on 4chanX.user.js
+++ Press '.' and do Quoting things on 4chanX.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Press Q and do Quoting things on 4chanX
+// @name         Press "." and do Quoting things on 4chanX
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Allows you to quote text without creating a quote link item in your post
 // @author      	ECHibiki - Sage
 // @match        *://boards.4chan.org/*
@@ -17,12 +17,14 @@ var qr_open = false;
 
 document.addEventListener("keydown", function(e){
 	if(e.key == THAT_FIELD_YOU_CHANGE_TO_SET_YOUR_HOTKEY_CHARACTER && !(document.activeElement.tagName == "INPUT" || document.activeElement.tagName == "TEXTAREA")){
-		var text = getSelectionText();
+		var text = getSelectionText().split("\n");
+        var intermed = text;
+        text = (text.map(function(line){return line.trim() != "" ? ">" + line : line;})).join("\n");
 		e.preventDefault();
 		e.stopPropagation();
 		if(!qr_open) document.getElementsByClassName("qr-link")[0].click();
 		setTimeout(function(){
-			document.getElementById("qr").getElementsByTagName("TEXTAREA")[0].value += ">" + text + "\n";
+			document.getElementById("qr").getElementsByTagName("TEXTAREA")[0].value += text + "\n";
 		},100);
 		return false;
 	}
