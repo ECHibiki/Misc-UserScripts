8805f26734b97b425c2a357a9b6fc543246cb779
ECHibiki <ECVerniy@gmail.com>
2018-03-17 03:00:55 -0400

    removed console.log

--- Press '.' and do Quoting things on 4chanX.js
+++ Press '.' and do Quoting things on 4chanX.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Press Q and do Quoting things on 4chanX
 // @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  try to take over the world!
+// @version      1.0
+// @description  Allows you to quote text without creating a quote link item in your post
 // @author      	ECHibiki - Sage
 // @match        *://boards.4chan.org/*
 // @grant        none
@@ -16,14 +16,11 @@ var THAT_FIELD_YOU_CHANGE_TO_SET_YOUR_HOTKEY_CHARACTER = ".";
 var qr_open = false;
 
 document.addEventListener("keydown", function(e){
-	console.log(document.activeElement.tagName );
-	console.log(document.activeElement);
 	if(e.key == THAT_FIELD_YOU_CHANGE_TO_SET_YOUR_HOTKEY_CHARACTER && !(document.activeElement.tagName == "INPUT" || document.activeElement.tagName == "TEXTAREA")){
 		var text = getSelectionText();
 		e.preventDefault();
 		e.stopPropagation();
 		if(!qr_open) document.getElementsByClassName("qr-link")[0].click();
-		console.log(document.getElementsByClassName("qr-link")[0]);
 		setTimeout(function(){
 			document.getElementById("qr").getElementsByTagName("TEXTAREA")[0].value += ">" + text + "\n";
 		},100);
